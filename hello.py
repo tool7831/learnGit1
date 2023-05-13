c32b536609cf4ad451f2f906c8e0ea2be5088fe0
JH Lee <tool7831@gmail.com>
2023-05-13 14:23:59 +0900

    add function print_git

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
+import sys
+
 def print_message():
     message = "world"
     print("hello {}".format(message))
+
+def print_git():
+    message = "from the hell"
+    sys.print(f"git {message}")
+
 
 if __name__-- ""__main__"":
     print_message()
